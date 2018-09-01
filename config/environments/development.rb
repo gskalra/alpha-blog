3f03621c731ccf6c3beffcbe6511a88dbd6ef5c8
Gurpal Singh ROR <gurpalsinghkalra@gmail.com>
2018-09-01 15:59:33 +0000

    whitelisting ip in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  #whitelist ip addresses
+  config.web_console.whitelisted_ips = '58.182.198.240'
 end
+
+
+
